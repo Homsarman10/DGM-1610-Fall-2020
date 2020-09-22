187a7dc9cb52becd5f3386ded53a0ae2bb1f7343
Michaelwells <michaelwells.email@gmail.com>
2020-09-21 20:02:57 -0600

    Added health, armor, and potatoes power ups commit

---
+++ Unity Project 0/Assets/Scripts/ArmorPowerUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmorPowerUp : MonoBehaviour
+{
+    public int armor;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        armor++;
+        print(armor);
+    }
+}
---
+++ Unity Project 0/Assets/Scripts/PotatoPowerUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotatoPowerUp : MonoBehaviour
+{
+    public int potatoes;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        potatoes++;
+            print(potatoes);
+    }
+}
