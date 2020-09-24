99d3f0a61463fffd0309fe253dada5906fea2486
Michaelwells <michaelwells.email@gmail.com>
2020-09-24 17:40:06 -0600

    Added rotation script, learned vector3 and if statements

---
+++ Unity Project 0/Assets/Scripts/Rotation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float speed = 10f;
+    public Vector3 rotationVector3;
+    private void Update()
+    {
+        rotationVector3.x = speed * Time.deltaTime;
+        rotationVector3.y = speed * Time.deltaTime;
+        rotationVector3.z = speed * Time.deltaTime;
+        transform.Rotate (rotationVector3);
+    }
+}
