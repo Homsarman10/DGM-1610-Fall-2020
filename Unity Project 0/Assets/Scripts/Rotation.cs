29e5c230a7fab4420eebdc986d227d5c27605e81
Michaelwells <michaelwells.email@gmail.com>
2020-09-24 18:00:34 -0600

    added if statement commit

--- Unity Project 0/Assets/Scripts/Rotation.cs
+++ Unity Project 0/Assets/Scripts/Rotation.cs
@@ -6,6 +6,10 @@ public class Rotation : MonoBehaviour
     public Vector3 rotationVector3;
     private void Update()
     {
+        if (rotationVector3.y > .8f)
+        {
+            print("I did it");
+        }
         rotationVector3.x = speed * Time.deltaTime;
         rotationVector3.y = speed * Time.deltaTime;
         rotationVector3.z = speed * Time.deltaTime;
