bb988aefab7ba85f115c70cbc0b662a08ca7fc2c
Michaelwells <michaelwells.email@gmail.com>
2020-09-29 20:39:32 -0600

    Added Unity Events Commit

--- Unity Project 0/Assets/Scripts/PowerUp.cs
+++ Unity Project 0/Assets/Scripts/PowerUp.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PowerUp : MonoBehaviour
 {
+    public UnityEvent powerUpEvent;
     public int score;
     private void OnTriggerEnter(Collider other)
     {
         score++;
         print(score);
+        powerUpEvent.Invoke();
     }
 }
 
