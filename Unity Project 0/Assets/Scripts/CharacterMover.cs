b6e838e388791929b21f91bdeccdffde3b1b9e2b
Michaelwells <michaelwells.email@gmail.com>
2020-10-05 22:57:12 -0600

    Added jump and input horizontal commit

--- Unity Project 0/Assets/Scripts/CharacterMover.cs
+++ Unity Project 0/Assets/Scripts/CharacterMover.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class CharacterMover : MonoBehaviour
 {
     public CharacterController controller;
-    public Vector3 moveDirection;
+    public float moveSpeed = 3f, gravity = -9.81f, jumpForce = 30f;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    private Vector3 moveDirection;
+    private float yDirection;
+    
     void Update()
     {
-        var movement = moveDirection * Time.deltaTime;
-        controller.Move(movement);
+        var moveSpeedInput = moveSpeed * Input.GetAxis("Horizontal");
+        moveDirection.Set(moveSpeedInput,yDirection,0);
+
+        yDirection += gravity * Time.deltaTime;
+
+        if (controller.isGrounded && moveDirection.y < 0)
+        {
+            yDirection = -1f;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            yDirection += jumpForce;
+        }
+        
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
