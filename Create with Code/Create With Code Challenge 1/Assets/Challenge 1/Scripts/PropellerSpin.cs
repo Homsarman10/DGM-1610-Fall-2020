2229cda2b4760aebd45df9804504467e6a3af65e
Michaelwells <michaelwells.email@gmail.com>
2020-10-06 17:16:18 -0600

    Added create with code projects finally commit

---
+++ Create with Code/Create With Code Challenge 1/Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+
+{
+    public float rotateSpeed = 15f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+    }
+}
---
+++ Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // private variables
+    private float speed = 15.0f;
+    private float turnSpeed = 30.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    //player input
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+        // We'll take Bikini Bottom and push it somewhere else!
+        transform.Translate (Vector3.forward * (Time.deltaTime * speed * forwardInput));
+        transform.Rotate(Vector3.up * (Time.deltaTime * turnSpeed * horizontalInput));
+    }
+}
